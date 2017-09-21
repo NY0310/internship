0345ff35365d7951e1f86873c3a7c6ebeea7c65f
SuguruYamashita <30929340+SuguruYamashita@users.noreply.github.com>
2017-09-21 20:35:34 +0900

    ステージ選択、タイトルからの遷移の最低限要素を追加

--- Ateam/Assets/Scenes/Footer/FooterSceneChange.cs
+++ Ateam/Assets/Scenes/Footer/FooterSceneChange.cs
@@ -8,6 +8,13 @@ public class FooterSceneChange : MonoBehaviour {
     public string SceneName;
     static string currentSceneName = "";
 
+    public static void FirstLoad(string sceneName)
+    {
+        currentSceneName = sceneName;
+        SceneManager.LoadScene("Footer", LoadSceneMode.Single);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
     public void ClickedEvent()
     {
         if (currentSceneName == SceneName) return;
