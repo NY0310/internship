76f7c1200a62d79807870158c0d9f893e09b8609
SuguruYamashita <30929340+SuguruYamashita@users.noreply.github.com>
2017-09-22 09:12:52 +0900

    HPの概念の追加。回復、ダメージを受けるように

--- Ateam/Assets/Battle/Scripts/BattleManager.cs
+++ Ateam/Assets/Battle/Scripts/BattleManager.cs
@@ -9,6 +9,26 @@ using UnityEngine;
 public class BattleManager : MonoBehaviour {
 
     public tDropLaneBase dropLane;
+    public HP PlayerHP;
+
+    enum State
+    {
+        PLAYER_ATTACK,
+        ENEMY_ATTACK
+    }
+    State state;
+
+    const float PLAYER_ATTACK_TIME = 5.0f;
+    float playerAttackRemainingTime = PLAYER_ATTACK_TIME;
+
+
+    ///　/////　　　仮　　　/////////////
+    void Start()
+    {
+        PlayerHP.Init(500f);    
+    }
+
+    /// /////////////////////////////////////////////////////    ユーザー入力処理    /////////////////////////////////////////////
 
     /// <summary>
     /// なぜTap～という関数を３つも作ったか
@@ -30,11 +50,80 @@ public class BattleManager : MonoBehaviour {
     }
     void UserInput(tDrop.Type type)
     {
-        dropLane.DestroyUnderDrop(type);
+        if (state == State.PLAYER_ATTACK)
+        {
+            int destroyNum = dropLane.DestroyUnderDrop(type);
+            playerAttackRemainingTime += destroyNum * 0.05f;
+        }
     }
 
+
+    /// ///////////////////////////////////////////////////    更新処理    //////////////////////////////////////////////////////
+
+    bool enemyAttacked = false; // 仮
     void Update()
     {
-        dropLane.DestroyIfUnderDropsAreSame();
+        UpdateState();
+        switch (state)
+        {
+            case State.PLAYER_ATTACK:
+                UpdatePlayerAttack();
+                break;
+            case State.ENEMY_ATTACK:
+                if (enemyAttackRemainingTime < 0.8f && !enemyAttacked)
+                {
+                    PlayerHP.Damaged(150f);
+                    enemyAttacked = true;
+                }
+                break;
+        }
     }
+
+    void UpdatePlayerAttack()
+    {
+        if (dropLane.DestroyIfUnderDropsAreSame())  // 3つ同時消しした時の処理
+        {
+            playerAttackRemainingTime += 0.2f;
+            PlayerHP.Recovery(40f);
+        }
+    }
+
+    float enemyAttackRemainingTime; // 仮
+    void UpdateState()
+    {
+        switch (state)
+        {
+            case State.PLAYER_ATTACK:
+                playerAttackRemainingTime -= Time.deltaTime;
+                if (playerAttackRemainingTime <= 0)
+                {
+                    ChangeState(State.ENEMY_ATTACK);
+                }
+                break;
+            case State.ENEMY_ATTACK:
+                enemyAttackRemainingTime -= Time.deltaTime;
+                if (enemyAttackRemainingTime <= 0)
+                {
+                    ChangeState(State.PLAYER_ATTACK);
+                }
+                break;
+        }
+    }
+
+    void ChangeState(State state)
+    {
+        switch (state)
+        {
+            case State.PLAYER_ATTACK:
+                playerAttackRemainingTime = PLAYER_ATTACK_TIME;
+                this.state = State.PLAYER_ATTACK;
+                break;
+            case State.ENEMY_ATTACK:
+                enemyAttacked = false;
+                enemyAttackRemainingTime = 2.5f;
+                this.state = State.ENEMY_ATTACK;
+                break;
+        }
+    }
+
 }
---
+++ Ateam/Assets/Battle/Scripts/HP/HP.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour {
+    float max;
+    float lowerHp; // 緑
+    float upperHp; // 赤
+    float hp;
+
+    public GameObject Bar;
+    public GameObject RedBar;
+
+    public void Init(float max)
+    {
+        this.max = max;
+        hp = max;
+        upperHp = hp;
+    }
+
+    public void Damaged(float power)
+    {
+        upperHp = hp;
+        hp -= power;
+        hp = Mathf.Clamp(hp,0f,max);
+        lowerHp = hp;
+    }
+    public void Recovery(float power)
+    {
+        lowerHp = hp;
+        hp += power;
+        hp = Mathf.Clamp(hp, 0f, max);
+        upperHp = hp;
+    }
+
+    void Update()
+    {
+        lowerHp += (hp - lowerHp) * 0.05f;
+        upperHp += (hp - upperHp) * 0.05f;
+        Bar.transform.localScale = new Vector3(lowerHp/max, 1f, 1f);
+        RedBar.transform.localScale = new Vector3(upperHp/max, 1f, 1f);
+    }
+
+}
