48c7031cb7ec91041e71fb4ca95a4ae2657e8ccd
SuguruYamashita <30929340+SuguruYamashita@users.noreply.github.com>
2017-09-26 04:42:13 +0900

    スタミナ性

---
+++ Ateam/Assets/Battle/Scripts/AttackButton/AttackButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackButton : MonoBehaviour {
+
+    float remaining;
+    public float MaxRemaining;
+    public float Recover;
+    float defaultHeight;
+    RectTransform rect;
+
+    BattleManager battleManager;
+
+	void Start () {
+        battleManager = GameObject.Find("BattleManager").GetComponent<BattleManager>();
+        rect = GetComponent<RectTransform>();
+        defaultHeight = rect.sizeDelta.y;
+        remaining = MaxRemaining;
+	}
+
+    void AddRemaining(float add)
+    {
+        remaining += add;
+        remaining = Mathf.Clamp(remaining, 0f, MaxRemaining);
+    }
+
+    public void TapCross()
+    {
+        if (battleManager.IsPlayerTurn() && remaining >= 1f)
+        {
+            battleManager.TapCross();
+            AddRemaining(-1f);
+        }
+    }
+    public void TapCircle()
+    {
+        if (battleManager.IsPlayerTurn() && remaining >= 1f)
+        {
+            battleManager.TapCircle();
+            AddRemaining(-1f);
+        }
+    }
+    public void TapTryangle()
+    {
+        if (battleManager.IsPlayerTurn() && remaining >= 1f)
+        {
+            battleManager.TapTryangle();
+            AddRemaining(-1f);
+        }
+    }
+
+    void Update () {
+        AddRemaining(Recover*Time.deltaTime);
+        rect.sizeDelta = new Vector2( rect.sizeDelta.x, defaultHeight * remaining / MaxRemaining);
+	}
+}
